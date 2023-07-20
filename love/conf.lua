092368d8a460bd67a3a392aeb8aebfa97da4c00c
soupayan909 <131850623+soupayan909@users.noreply.github.com>
2023-07-20 19:38:40 +0530

    Update conf.lua

--- love/conf.lua
+++ love/conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.width = 1024
     t.window.height = 768
     
-    t.window.title = "ActiveMemory"
+    t.window.title = "ActiveMind"
     t.window.resizable = true
     t.window.icon = 'images/icon.png'
 end
