46e8adf43f9896b5d3cfc2340ee7dd935c6a5a2f
soupayan909 <131850623+soupayan909@users.noreply.github.com>
2023-07-20 19:38:22 +0530

    Update conf.lua

--- love/conf.lua
+++ love/conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.window.width = 1024
     t.window.height = 768
-    --[[t.window.title = "Suprememory!"]]
+    
     t.window.title = "ActiveMemory"
     t.window.resizable = true
     t.window.icon = 'images/icon.png'
